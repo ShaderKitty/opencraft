82fdab247dd5fb3ac97bb210e7d9a27a5e0e3cbc
ShaderKitty <ajossart3@gmail.com>
2021-11-21 13:01:39 -0500

    opencraft

--- src/opencraft.cpp
+++ src/opencraft.cpp
@@ -62,22 +62,40 @@ opencraft::~opencraft() {
 
 void opencraft::run() {
 
-	//Window1 = new system_window(
-	//	this->Engine,
-	//	Context,
-	//	SystemDisplay,
-	//	WindowProperty,
-	//	SwapchainProperty,
-	//	VkFormat::VK_FORMAT_B8G8R8A8_SRGB,
-	//	640,
-	//	480,
-	//	"cock"
-	//);
+	system_display* SystemDisplay = this->Engine->get_primary_display();
+	window::prop WindowProperty = window::prop();
+	swapchain::prop SwapchainProperty = swapchain::prop();
+
+	WindowProperty.UserFocused = GLFW_FALSE;
+
+
+	SwapchainProperty.Count = 3;
+	SwapchainProperty.ColorSpace = VkColorSpaceKHR::VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+	SwapchainProperty.Usage = texture::usage::COLOR_ATTACHMENT;
+	SwapchainProperty.CompositeAlpha = swapchain::composite::ALPHA_OPAQUE;
+	SwapchainProperty.PresentMode = swapchain::mode::FIFO;
+	SwapchainProperty.Clipped = true;
+
+	system_window *Window2 = new system_window(
+		this->Engine,
+		Context,
+		SystemDisplay,
+		WindowProperty,
+		SwapchainProperty,
+		VkFormat::VK_FORMAT_B8G8R8A8_SRGB,
+		640,
+		480,
+		"cock"
+	);
 
 	// App loop
 	while (!Engine->Shutdown.load()) {
 
 		core::logic::waitfor(3);
-		//Engine->Shutdown.store(true);
+		Engine->Shutdown.store(true);
 	}
+
+	delete Window2;
+
+
 }
